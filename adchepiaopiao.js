97ba28f221403a0abe535f39738e2c4d9ec5b087
boolen35 <122870851+boolen35@users.noreply.github.com>
2023-08-03 14:32:45 +0800

    Update adchepiaopiao.js

--- adchepiaopiao.js
+++ adchepiaopiao.js
@@ -8,19 +8,19 @@ hostname = api.revenuecat.com
 *************************************/
 
 
-const chxm1023 = {};
+const boolen35 = {};
 const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 if (typeof $response == "undefined") {
   delete $request.headers["x-revenuecat-etag"];
   delete $request.headers["X-RevenueCat-ETag"];
-  chxm1023.headers = $request.headers;
+  boolen35.headers = $request.headers;
 } else if (chxm1024 && chxm1024.subscriber) {
   chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
   chxm1024.subscriber.entitlement = chxm1024.subscriber.entitlement || {};
   const app = 'cm';const list = {'cm':{name: 'vip', id: 'eticket_3_life'}};
   const data = {
-   "Author": "chxm1023",
-   "Telegram" : "https://t.me/chxm1023",
+   "Author": "boolen35",
+   "Telegram" : "https://t.me/boolen35",
    "warning": "仅供学习，禁止转载或售卖",
    "original_purchase_date": "2022-09-09T09:09:09Z",
    "purchase_date": "2022-09-09T09:09:09Z",
@@ -32,7 +32,7 @@ for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
                 break;
           }
     }
-    chxm1023.body = JSON.stringify(chxm1024);
+    boolen35.body = JSON.stringify(chxm1024);
 }
 
-$done(chxm1023);
+$done(boolen35);
